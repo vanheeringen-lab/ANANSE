5c79a35092ae2481ee1b2cfa892083ec2ded70c1
qxu <qxu@mb07.science.ru.nl>
2019-10-07 18:18:23 +0200

    binding function to python class

---
+++ grns/__init__.py
@@ -0,0 +1,28 @@
+import atexit
+from os import getpid
+import shutil
+from tempfile import mkdtemp
+import logging
+
+def mytmpdir():
+    if not hasattr(mytmpdir, 'dir') or not mytmpdir.dir:
+        mytmpdir.dir = mkdtemp(prefix="gimmemotifs.{0}.".format(getpid()))
+        atexit.register(shutil.rmtree, mytmpdir.dir)
+    return mytmpdir.dir
+
+logger = logging.getLogger('gimme')
+logger.setLevel(logging.DEBUG)
+logger.propagate = 0
+
+# nice format
+screen_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+# Log to screen
+sh = logging.StreamHandler()
+sh.setLevel(logging.INFO)
+sh.setFormatter(screen_formatter)
+logger.addHandler(sh)
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
