8cfd2d5452387be689bffae1eca144bed5657658
qxu <qxu@mb07.science.ru.nl>
2019-10-11 16:52:11 +0200

    add interaction command

---
+++ grns/commands/interaction.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python 
+# Copyright (c) 2009-2019 Quan Xu <qxuchn@gmail.com>
+#
+# This module is free software. You can redistribute it and/or modify it under 
+# the terms of the MIT License, see the file COPYING included with this 
+# distribution.
+
+from __future__ import print_function
+import sys
+import os
+
+import grns.interaction 
+import grns.config as cfg
+
+def interaction(args):
+    config = cfg.MotifConfig()
+    params = config.get_default_params()
+
+    if not os.path.exists(args.fin_rpkm):
+        print("File %s does not exist!" % args.fin_rpkm)
+        sys.exit(1)
+    
+    params = {
+        # "pwmfile": args.pwmfile,
+        # "fin_rpkm": args.fin_rpkm,
+        # "outfile": args.outfile,
+        # "genome": args.genome,
+        # "gene_bed": args.annotation,
+        # "fpomoter": args.fpomoter,
+        # "detail": args.detail,
+        
+        "fin_rpkm": args.fin_rpkm,
+        "pwmfile": args.pwmfile,
+        "fin_expression": args.fin_expression,
+        "outfile": args.outfile,
+        "genome": args.genome,
+        "gene_bed": args.annotation,
+        "corrfiles": args.corrfiles,
+        "binding": args.binding,
+        
+    }
+
+    b = grns.interaction.Interaction(genome = args.genome, gene_bed = args.annotation, pwmfile = args.pwmfile)
+    b.run_interaction(args.fin_rpkm, args.binding, args.outfile)
--- grns/interaction.py
+++ grns/interaction.py
@@ -110,9 +110,9 @@ class Interaction(object):
                                 "dist" : range(keep1+1, padding+1)})
         weight = pd.concat([weight1, weight2, weight3])
 
-        weightfile = NamedTemporaryFile(mode="w", dir=mytmpdir())
+        weightfile = NamedTemporaryFile(mode="w", dir=mytmpdir(),delete=False)
         weight.to_csv(weightfile)
-        return(weightfile)
+        return(weightfile.name)
 
     def aggregate_binding(self, binding, prom, p, weight):
 
@@ -129,7 +129,7 @@ class Interaction(object):
         f_table["sum_weighted_logodds"] = f_table["binding"].div(f_table["binding"].mean()).apply(np.log, meta=('binding', np.float64)).rmul(50000).div(f_table["dist"])
         f_table["sum_logodds"] = f_table["binding"].div(f_table["binding"].mean()).apply(np.log, meta=('binding', np.float64))
 
-        weight=dd.read_csv(os.path.join(outdir, "weight.csv"))
+        weight=dd.read_csv(weight)
         f_table=f_table.merge(weight,how='left',on='dist')
         f_table['sum_dist_weight'] = f_table['binding'] * f_table['weight']
 
@@ -172,134 +172,173 @@ class Interaction(object):
         f_table["gene"] = f_table["source_target"].str.replace(".*_", '')
         f_table["max_binding_in_promoter"] = f_table["max_binding_in_promoter"].fillna(0)
 
-        outfile = os.path.join(outdir, "features.h5")
-        print("computing, output file {}".format(outfile))
-        f_table.to_hdf(outfile, key="/features")
+        # outfile = os.path.join(outdir, "features.h5")
+        features_file = NamedTemporaryFile(mode="w", dir=mytmpdir(),delete=False)
+        print("computing, output file {}".format(features_file.name))
+        f_table.to_hdf(features_file.name, key="/features")
+        return(features_file.name)
         # f_table.to_csv(outfile.replace("h5","txt"), sep="\t", index=False)
 
-def main():
-    gene_bed="/home/qxu/.local/share/genomes/hg38/hg38_gffbed_piroteinCoding.bed"
-    peak_bed="data/krt_enhancer.bed"
-    pwmfile="../data/gimme.vertebrate.v5.1.pfm"
-    binding="results/binding.predicted.h5"
+    def get_expression(self, fin_expression, features, min_tpm=1e-10, column="tpm"):
+        df = pd.read_hdf(features)
+        df=df[["source_target", "factor", "gene"]]
+        df.source_target=[i.upper() for i in list(df.source_target)]
+        df.gene=[i.upper() for i in list(df.gene)]
+        df = df.set_index("source_target")
+        # fa2name={}
+        # fa2=open("/home/qxu/projects/regulatoryNetwork/run20180716/scripts/data/gene2name.txt","r")
 
-    b=grns.interaction.Interaction(genome="hg38", gene_bed= gene_bed, pwmfile=pwmfile)
-    prom=b.get_promoter_dataframe(peak_bed)
-    p=b.get_gene_dataframe(peak_bed)
-    weight=b.distance_weight()
+        # #switch the Factor name to gene name
+        # for i in fa2:
+        #     a=i.split()
+        #     if a[0].startswith("gene"):
+        #         fa2name[a[1]]=a[0]
 
-    b.aggregate_binding(binding, prom, p, weight)
+        # flist=[]
+        # for f in list(df["factor"]):
+        #     if str.lower(f) in fa2name:
+        #         flist.append(fa2name[str.lower(f)])
+        #     elif f in fa2name:
+        #         flist.append(fa2name[f])
+        #     else:
+        #         flist.append("")
+        # df["gfactor"]=flist
 
+        # Take mean of all TPMs
+        expression = pd.DataFrame(
+                pd.concat([
+                    pd.read_table(f, index_col=0)[[column]] for f in fin_expression],
+                    axis=1).mean(1), 
+                columns=[column])
+        expression.index=[i.upper() for i in list(expression.index)]
+        # print(expression)
+        expression[column] = np.log2(expression[column] + 1e-5)
+        df = df.join(expression, on="factor")
+        df = df.rename(columns={column:"factor_expression"})
+        df = df.join(expression, on="gene")
+        df = df.rename(columns={column:"target_expression"})
 
+        df = df.dropna()
 
-def get_expression(fin_expression, features, outdir, min_tpm=1e-10, column="tpm"):
-    df = pd.read_hdf(features)
-    df=df[["source_target", "factor", "gene"]]
-    df.source_target=[i.upper() for i in list(df.source_target)]
-    df.gene=[i.upper() for i in list(df.gene)]
-    df = df.set_index("source_target")
-    # fa2name={}
-    # fa2=open("/home/qxu/projects/regulatoryNetwork/run20180716/scripts/data/gene2name.txt","r")
+        for col in ["factor_expression", "target_expression"]:
+            df[col + ".scale"] = minmax_scale(df[col])
+            df[col + ".rank.scale"] = minmax_scale(rankdata(df[col]))
 
-    # #switch the Factor name to gene name
-    # for i in fa2:
-    #     a=i.split()
-    #     if a[0].startswith("gene"):
-    #         fa2name[a[1]]=a[0]
+        expression_file = NamedTemporaryFile(mode="w", dir=mytmpdir(),delete=False)
+        # outfile = os.path.join(outdir, "expression.txt")
+        df.to_csv(expression_file, sep="\t")
+        return(expression_file.name)
 
-    # flist=[]
-    # for f in list(df["factor"]):
-    #     if str.lower(f) in fa2name:
-    #         flist.append(fa2name[str.lower(f)])
-    #     elif f in fa2name:
-    #         flist.append(fa2name[f])
-    #     else:
-    #         flist.append("")
-    # df["gfactor"]=flist
+    def get_factorExpression(self, fin_expression):
+        import numpy as np
+        import pandas as pd
+        from scipy.stats import rankdata
+        from sklearn import preprocessing
+        import warnings
+        warnings.filterwarnings('ignore')
+        factorsExpression = {}
+        #for line in open('/home/george/data/cis-bp.vertebrate.clusters.v3.0.motif2factors.txt'):
+        for line in open(self.motifs2factors):
+            motif = line.split('\t')[0].upper()
+            if not line.split('\t')[1].strip().split(',') == ['']:
+                for factor in line.split('\t')[1].strip().split(','):
+                    factorsExpression[factor.upper()] = []
 
-    # Take mean of all TPMs
-    expression = pd.DataFrame(
-            pd.concat([
-                pd.read_table(f, index_col=0)[[column]] for f in fin_expression],
-                axis=1).mean(1), 
-            columns=[column])
-    expression.index=[i.upper() for i in list(expression.index)]
-    # print(expression)
-    expression[column] = np.log2(expression[column] + 1e-5)
-    df = df.join(expression, on="factor")
-    df = df.rename(columns={column:"factor_expression"})
-    df = df.join(expression, on="gene")
-    df = df.rename(columns={column:"target_expression"})
+        for f in fin_expression: 
+            with open(f) as fa:
+                for line in fa:
+                    if not line.startswith('target_id'):
+                        gene = line.split('\t')[0].upper()
+                        expression = float(line.split('\t')[1])
+                        if gene in factorsExpression:
+                            if expression < 1e-10:
+                                expression = 1e-10
+                            factorsExpression[gene].append(np.log10(expression))
 
-    df = df.dropna()
+        # for line in open(fin_b):
+        #     if not line.startswith('target_id'):
+        #         gene = line.split('\t')[0].upper()
+        #         expression = float(line.split('\t')[4])
+        #         if gene in factorsExpression:
+        #             if expression < 1e-10:
+        #                 expression = 1e-10
+        #             factorsExpression[gene].append(np.log10(expression))
 
-    for col in ["factor_expression", "target_expression"]:
-        df[col + ".scale"] = minmax_scale(df[col])
-        df[col + ".rank.scale"] = minmax_scale(rankdata(df[col]))
-
-    outfile = os.path.join(outdir, "expression.txt")
-    df.to_csv(outfile, sep="\t")
-
-def get_factorExpression(fin_expression, motifs2factors, outdir):
-    import numpy as np
-    import pandas as pd
-    from scipy.stats import rankdata
-    from sklearn import preprocessing
-    import warnings
-    warnings.filterwarnings('ignore')
-    factorsExpression = {}
-    #for line in open('/home/george/data/cis-bp.vertebrate.clusters.v3.0.motif2factors.txt'):
-    for line in open(motifs2factors):
-        motif = line.split('\t')[0].upper()
-        if not line.split('\t')[1].strip().split(',') == ['']:
-            for factor in line.split('\t')[1].strip().split(','):
-                factorsExpression[factor.upper()] = []
-
-    for f in fin_expression: 
-        with open(f) as fa:
-            for line in fa:
-                if not line.startswith('target_id'):
-                    gene = line.split('\t')[0].upper()
-                    expression = float(line.split('\t')[1])
-                    if gene in factorsExpression:
-                        if expression < 1e-10:
-                            expression = 1e-10
-                        factorsExpression[gene].append(np.log10(expression))
-
-    # for line in open(fin_b):
-    #     if not line.startswith('target_id'):
-    #         gene = line.split('\t')[0].upper()
-    #         expression = float(line.split('\t')[4])
-    #         if gene in factorsExpression:
-    #             if expression < 1e-10:
-    #                 expression = 1e-10
-    #             factorsExpression[gene].append(np.log10(expression))
-    with open(os.path.join(outdir, "factorExpression.txt"), 'w') as fout:
-        fout.write('#factor\tfactorExpression\n')
+        factors_expression_file = NamedTemporaryFile(mode="w", dir=mytmpdir(),delete=False)
+        factors_expression_file.write('#factor\tfactorExpression\n')
         for factor in factorsExpression:
             if len(factorsExpression[factor]) == 0:
-                fout.write('{}\t{}\n'.format(factor, np.log10(1e-10)))
+                factors_expression_file.write('{}\t{}\n'.format(factor, np.log10(1e-10)))
             else:
-                fout.write('{}\t{}\n'.format(factor, np.mean(factorsExpression[factor])))
+                factors_expression_file.write('{}\t{}\n'.format(factor, np.mean(factorsExpression[factor])))
 
-    scores_df = pd.read_table(os.path.join(outdir, "factorExpression.txt"), sep="\t",index_col=0)
-    #scores_df['factorExpressionRank'] = preprocessing.MinMaxScaler().fit_transform(rankdata(scores_df['factorExpression'], method='average'))
-    scores_df['factorExpressionRank'] = preprocessing.MinMaxScaler().fit_transform(rankdata(scores_df['factorExpression'], method='average').reshape(-1,1))
-    scores_df.to_csv(os.path.join(outdir, "factorExpression.txt"), sep='\t')
+        scores_df = pd.read_table(factors_expression_file.name, sep="\t",index_col=0)
+        #scores_df['factorExpressionRank'] = preprocessing.MinMaxScaler().fit_transform(rankdata(scores_df['factorExpression'], method='average'))
+        scores_df['factorExpressionRank'] = preprocessing.MinMaxScaler().fit_transform(rankdata(scores_df['factorExpression'], method='average').reshape(-1,1))
 
-def get_correlation(corrfiles, features, outdir):
-    df = pd.read_hdf(features)
-    df=df[["source_target"]]
-    df.source_target=[i.upper() for i in list(df.source_target)]
-    df = df.set_index("source_target")
+        scores_df.to_csv(factors_expression_file.name, sep='\t')
+        return(factors_expression_file.name)
 
-    for i, corrfile in enumerate(corrfiles):
-        corr = pd.read_table(corrfile, sep="\t", index_col=0)
-        corr = corr.rename(columns={corr.columns[0]:"corr_file{}".format(i + 1)})
-        df = df.join(corr)
-    
-    outfile = os.path.join(outdir, "correlation.txt")
-    df.to_csv(outfile, sep="\t")
+    def get_correlation(self, corrfiles, features):
+        df = pd.read_hdf(features)
+        df=df[["source_target"]]
+        df.source_target=[i.upper() for i in list(df.source_target)]
+        df = df.set_index("source_target")
+
+        for i, corrfile in enumerate(corrfiles):
+            corr = pd.read_table(corrfile, sep="\t", index_col=0)
+            corr = corr.rename(columns={corr.columns[0]:"corr_file{}".format(i + 1)})
+            df = df.join(corr)
+        
+        corr_file = NamedTemporaryFile(mode="w", dir=mytmpdir(),delete=False)
+        # outfile = os.path.join(outdir, "correlation.txt")
+        df.to_csv(corr_file, sep="\t")
+        return(corr_file.name)
+
+    def join_features(self, features, other, outfile):
+        network = pd.read_hdf(features)
+
+        for fname in other:
+            df = pd.read_table(fname, sep="\t")
+            for col in ["factor", "gene"]:
+                if col in df.columns:
+                    df = df.drop(col, 1)
+            network = network.merge(df, 
+                    left_on="source_target", 
+                    right_on="source_target",)
+        
+        # Compute before saving, will result in an error otherwise
+        # network = network.compute()   
+        # outfile = NamedTemporaryFile(mode="w", dir=mytmpdir(),delete=False)
+        network.to_csv(outfile, sep="\t", index=False)
+        # network.to_hdf(outfile, key="/features")
+
+    def run_interaction(self, peak_bed, binding, outfile):
+        # gene_bed="/home/qxu/.local/share/genomes/hg38/hg38_gffbed_piroteinCoding.bed"
+        # peak_bed="data/krt_enhancer.bed"
+        # pwmfile="../data/gimme.vertebrate.v5.1.pfm"
+        # binding="results/binding.predicted.h5"
+
+        b=grns.interaction.Interaction(genome=self.genome, gene_bed= self.gene_bed, pwmfile=self.pwmfile)
+        prom=b.get_promoter_dataframe(peak_bed)
+        p=b.get_gene_dataframe(peak_bed)
+        weight = b.distance_weight()
+
+        features = b.aggregate_binding(binding, prom, p, weight)
+
+        expression_file = b.get_expression(fin_expression, features)
+        factors_expression_file = b.get_factorExpression(fin_expression)
+
+        corr_file = b.get_correlation(corrfiles, features)
+
+        other = [
+            expression_file,
+            corr_file,
+        ]
+        # outfile = 'full_features.h5'
+        b.join_features(features, other, outfile) 
+
+
 
 def get_peakRPKM(fin_rpkm, outdir):
     peaks = pd.read_table(fin_rpkm, 
@@ -335,23 +374,6 @@ def distance_weight(binding,distance,alpha=1e5,padding=int(2e5)):
     wbinding= np.dot( binding, weight[distance])
     return(wbinding)
 
-
-def join_features(features, other, outfile):
-    network = pd.read_hdf(features)
-
-    for fname in other:
-        df = pd.read_table(fname, sep="\t")
-        for col in ["factor", "gene"]:
-            if col in df.columns:
-                df = df.drop(col, 1)
-        network = network.merge(df, 
-                left_on="source_target", 
-                right_on="source_target",)
-    
-    # Compute before saving, will result in an error otherwise
-    # network = network.compute()   
-    network.to_csv(outfile.replace("h5","txt"), sep="\t", index=False)
-    network.to_hdf(outfile, key="/features")
 
 def calculate_features(gene_bed, outdir, binding, genome="hg19", pwmfile=None, fin_expression=None, corrfiles=None):
     if not os.path.exists(outdir):
