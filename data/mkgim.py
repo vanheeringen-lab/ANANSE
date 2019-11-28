6955959476692790222d20e2d40008571952b901
qxu <qxu@mb07.science.ru.nl>
2019-11-28 15:21:11 +0100

    clear data folder

--- data/mkgim.py
+++
@@ -1,8 +0,0 @@
-fl2=open("gimme.vertebrate.v3.3.1.xt.motif2factors.txt","w")
-with open("gimme.vertebrate.v3.3.xt.motif2factors.txt","r") as fl1:
-    for i in fl1:
-        a=i.split()
-        if len(a)==2:
-            fl2.write('{0}\t{1}\n'.format(a[0],a[1].upper()))
-        else:
-            fl2.write(i)
