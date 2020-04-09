26396fa68042f0d7669b3caf7f37306a30dffbb3
Quan Xu <qxuchn@gmail.com>
2020-04-09 21:23:04 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,9 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'rtd-dropdown'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
