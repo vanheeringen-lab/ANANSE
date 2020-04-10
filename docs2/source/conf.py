5def10bd6749a73c9187ea67af79ea0ec2055d64
Quan Xu <qxuchn@gmail.com>
2020-04-10 11:25:57 +0200

    docs

--- docs2/source/conf.py
+++ docs2/source/conf.py
@@ -62,12 +62,6 @@ from recommonmark.parser import CommonMarkParser
 source_parsers = {
     '.md': CommonMarkParser,
 }
-# source_suffix = ['.rst', '.md']
+source_suffix = ['.rst', '.md']
 
-
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
-}
 
