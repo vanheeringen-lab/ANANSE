ed0ba5e63155254c7943b1891c65c4ea0903a3bd
siebrenf <siebrenf@gmail.com>
2021-02-12 18:41:04 +0100

    full reimplementation - gotta test

--- ananse/enhancer_binding.py
+++ ananse/enhancer_binding.py
@@ -65,7 +65,7 @@ def is_narrowpeak(bed, check_values=True):
     return False
 
 
-def bed_width(genome, bed_in, bed_out, width=200, narrowpeak=False, fix_outliers=False, output_bed3=True, verbose=True):
+def bed_resize(genome, bed_in, bed_out, width=200, narrowpeak=False, fix_outliers=False, output_bed3=True, verbose=True):
     """
     Set bed region width.
 
@@ -157,7 +157,7 @@ class CombineBedFiles:
                     resized_peakfile = os.path.join(tmpdir, os.path.basename(peakfile))
 
                     # resize each BED region to 200 BP
-                    bed_width(
+                    bed_resize(
                         genome=self.genome,
                         bed_in=peakfile,
                         bed_out=resized_peakfile,
@@ -205,26 +205,26 @@ class CombineBamFiles:
                 shutil.rmtree(tmpdir, ignore_errors=True)
 
 
-def bed_sum_coverages(multi_bam_coverage, sum_bam_coverage):
-    """
-    MultiBamCov returns a BED3+n with one column per bam.
-    This function sums up all bam counts and returns a BED3+1.
-    """
-    bed = pd.read_csv(multi_bam_coverage, header=None, sep="\t")
-    columns = bed.shape[1]
-    if columns != 4:
-        bed3 = bed.iloc[:, :3]
-        scores = bed.iloc[:, 3:].sum(axis=1)
-        bed = pd.concat([bed3, scores], axis=1)
-    bed.to_csv(sum_bam_coverage, sep="\t", header=False, index=False)
+# def bed_sum_coverages(multi_bam_coverage, sum_bam_coverage):
+#     """
+#     MultiBamCov returns a BED3+n with one column per bam.
+#     This function sums up all bam counts and returns a BED3+1.
+#     """
+#     bed = pd.read_csv(multi_bam_coverage, header=None, sep="\t")
+#     columns = bed.shape[1]
+#     if columns != 4:
+#         bed3 = bed.iloc[:, :3]
+#         scores = bed.iloc[:, 3:].sum(axis=1)
+#         bed = pd.concat([bed3, scores], axis=1)
+#     bed.to_csv(sum_bam_coverage, sep="\t", header=False, index=False)
 
 
-def bed_normalize(bam_coverage, bed_output, dist_func="lognorm_dist", **kwargs):
+def normalize_peaks(bam_coverage, bed_output, dist_func="lognorm_dist", **kwargs):
     """
     Fit the bam coverage scores to a distribution
     """
     bed = pd.read_csv(bam_coverage, header=None, sep="\t")
-    peak = (bed[0] + ":" + bed[1].astype(str) + "-" + bed[2].astype(str))
+    region = (bed[0] + ":" + bed[1].astype(str) + "-" + bed[2].astype(str))
     score = bed[3]
 
     # obtain a distribution
@@ -238,15 +238,16 @@ def bed_normalize(bam_coverage, bed_output, dist_func="lognorm_dist", **kwargs):
 
     logn_score = np.log(norm_score + 1)
     scaled_score = minmax_scale(logn_score)
+    log10_score = np.log10(norm_score + 1)
 
     data = {
-        "peak": peak,
+        "region": region,  # ex: "chr1:0-200"
         "score": score,
         "norm_score": norm_score,
         "logn_score": logn_score,
         "scaled_score": scaled_score,
+        "log10_score": log10_score,  # used by the original function
     }
-
     bed = pd.DataFrame(data=data)
     bed.to_csv(bed_output, sep="\t", index=False)
 
@@ -276,21 +277,22 @@ class ScorePeaks:
             self.count_reads(coverage_file)
 
         # fit bam read counts to specified distribution
-        bed_normalize(coverage_file, outfile, dist_func=dist_func, **kwargs)
+        normalize_peaks(coverage_file, outfile, dist_func=dist_func, **kwargs)
 
 
 class ScoreMotifs:
     def __init__(self, genome, bed, pfmfile=None, ncore=1, verbose=True):
         self.genome = genome
         self.bed = bed  # one bed file with putative enhancer binding regions
-        self.pfmfile = pfmfile_location(pfmfile)
-        self.motifs2factors = self.pfmfile.replace(".pfm", ".motif2factors.txt")
+        self.pfm_file = pfmfile_location(pfmfile)
+        # self.motifs2factors_file = self.pfm_file.replace(".pfm", ".motif2factors.txt")
+        # self.motifs2factors = self.filter_transcription_factors(*args, **kwargs)
         self.ncore = ncore
         self.verbose = verbose
 
     def setup_gimme_scanner(self):
         s = Scanner(ncpus=self.ncore)
-        s.set_motifs(self.pfmfile)
+        s.set_motifs(self.pfm_file)
         s.set_genome(self.genome)
 
         # generate GC background index
@@ -305,90 +307,170 @@ class ScoreMotifs:
         """
         scanner = self.setup_gimme_scanner()
 
-        with open(self.pfmfile) as f:
+        with open(self.pfm_file) as f:
             motifs = read_motifs(f)
 
         # new file with header only (append data in chunks)
         with open(pfmscorefile, 'w') as f:
             # Quan: When we built model, rank and minmax normalization was used.
-            cols = ["enhancer", "zscore", "zscoreRank"]
+            cols = ["motif", "region", "zscore"]  # , "zscoreRank"]
             f.write("\t".join(cols)+"\n")
 
         seqs = [s.split(" ")[0] for s in as_fasta(enhancer_regions_bed, genome=self.genome).ids]
         with tqdm(total=len(seqs), unit="seq") as pbar:
             # Run 10k regions per scan.
-            chunksize = 10000
+            chunksize = 10_000
             for chunk in range(0, len(seqs), chunksize):
                 pfm_score = []
                 chunk_seqs = seqs[chunk:chunk+chunksize]
                 # We are using GC-normalization for motif scanning as many enhancer binding regions are GC-enriched.
                 chunk_scores = scanner.best_score(chunk_seqs, zscore=True, gc=True)
+                # for each seq, store the score of each motif
                 for seq, scores in zip(chunk_seqs, chunk_scores):
                     for motif, score in zip(motifs, scores):
                         pfm_score.append([motif.id, seq, score])
                     pbar.update(1)
-                pfm_score = pd.DataFrame(pfm_score, columns=["motif", "enhancer", "zscore"])
-                pfm_score = pfm_score.set_index("motif")
-                pfm_score["zscoreRank"] = minmax_scale(stats.rankdata(pfm_score["zscore"]))
+                pfm_score = pd.DataFrame(pfm_score, columns=cols)  # ["motif", "region", "zscore"])
+
+                # TODO: should this be a separate function (more modular but way slower)? See "normalize_motifs"
+                # pfm_score["zscoreRank"] = minmax_scale(stats.rankdata(pfm_score["zscore"]))
 
                 pfm_score[cols].to_csv(pfmscorefile, sep="\t", header=False, mode='a')
 
+    def normalize_motifs(self, bed_input, bed_output):
+        """
+        Add normalized scores to the scored motifs
+        """
+        bed = dd.read_csv(bed_input, sep="\t")
+        bed["rank_zscore"] = minmax_scale(stats.rankdata(bed["zscore"]))
+        bed.to_csv(
+            bed_output, sep="\t",  # to_csv args
+            single_file=True, compute_kwargs={"num_workers": self.ncore}  # dask args
+            )
+
+    # def run(self, outfile, force=False):
+    #     if force or not os.path.exists(outfile):
+    #         tmpdir = tempfile.mkdtemp()
+    #         try:
+    #             if self.verbose:
+    #                 print("Running gimme scan (really slow)")
+    #             pfm_weight = os.path.join(tmpdir, "peak_weight")
+    #             self.get_motif_scores(self.bed, pfm_weight)
+    #
+    #             shutil.copy2(pfm_weight, outfile)
+    #         finally:
+    #             shutil.rmtree(tmpdir, ignore_errors=True)
+
     def run(self, outfile, force=False):
-        if force or not os.path.exists(outfile):
+        raw_motif_scores = os.path.join(os.path.dirname(outfile), "raw_motif_scores.bed")
+        if force or not os.path.exists(raw_motif_scores):
             tmpdir = tempfile.mkdtemp()
             try:
                 if self.verbose:
                     print("Running gimme scan (really slow)")
-                pfm_weight = os.path.join(tmpdir, "peak_weight")
-                self.get_motif_scores(self.bed, pfm_weight)
+                tmp_motif_scores = os.path.join(tmpdir, "motif_scores")
+                self.get_motif_scores(self.bed, tmp_motif_scores)
 
-                shutil.copy2(pfm_weight, outfile)
+                shutil.copy2(tmp_motif_scores, raw_motif_scores)
             finally:
                 shutil.rmtree(tmpdir, ignore_errors=True)
 
+        if force or not os.path.exists(outfile):
+            self.normalize_motifs(raw_motif_scores, outfile)
+
+    # def run(self, outfile, dist_func="lognorm_dist", force=False, **kwargs):
+    #     # save the results as it takes ages to run
+    #     coverage_file = os.path.join(os.path.dirname(outfile), "raw_enhancer_coverage.bed")
+    #     if force or not os.path.exists(coverage_file):
+    #         if self.verbose:
+    #             print("Running bam coverage (slow)")
+    #         self.count_reads(coverage_file)
+    #
+    #     # fit bam read counts to specified distribution
+    #     normalize_peaks(coverage_file, outfile, dist_func=dist_func, **kwargs)
+
 
 class Binding:
-    def __init__(self, peak_weights, motif_weights, model=None, ncore=1, verbose=True):
+    def __init__(self, peak_weights, motif_weights, pfmfile=None, model=None, ncore=1, verbose=True, *args, **kwargs):
         self.peak_weights = peak_weights  # output from ScorePeaks
         self.motif_weights = motif_weights  # output from ScoreMotifs
+
+        self.pfm_file = pfmfile_location(pfmfile)
+        self.motifs2factors_file = self.pfm_file.replace(".pfm", ".motif2factors.txt")
+        self.motifs2factors = self.filter_transcription_factors(*args, **kwargs)
+
         self.model = model
         if self.model is None:
             # dream_model.txt is a 2D logistic regression model.
             package_dir = os.path.dirname(__file__)
             self.model = os.path.join(package_dir, "db", "dream_model_p300.pickle")
+
         self.ncore = ncore
         self.verbose = verbose
+
+    def filter_transcription_factors(self, curation_filter=None, tf_list=None, whitelist=True):
+        """
+        filter transcription factors from the motif database
+
+        curation_filter: If None (default), keep all factors.
+        If True, keep only curated factors. If False, keep only non-curated factors.
+        Note: "Curated" TFs have direct evidence for binding or are manually selected for likely binding.
+
+        tf_list: an optional, single-column file with (case-insensitive) transcription factor names.
+        whitelist: if True (default), tf_list is used as a whitelist. If False, as a blacklist.
+        """
+        m2f = pd.read_csv(self.motifs2factors_file, sep="\t")
+
+        # rename stuff
+        m2f.rename(columns={"Motif": "motif", "Factor": "factor", "Curated": "curated"}, inplace=True)
+        m2f['factor'] = m2f.factor.str.upper()  # make case-insensitive
+        m2f.replace("T", "TBXT", inplace=True)  # rename T to TBXT
+
+        # filter by curation
+        if curation_filter is True:
+            m2f = m2f.loc[m2f.curated == "Y"]
+        elif curation_filter is False:
+            m2f = m2f.loc[m2f.curated == "N"]
+
+        # shrink table
+        m2f = m2f[["motif", "factor"]]  # subset
+        m2f.drop_duplicates(inplace=True)  # case-insensitivity adds loads of duplicates (Sox9 and SOX9)
+
+        # filter by white/blacklist
+        if tf_list:
+            tfs = pd.read_csv(tf_list, header=None)[0].str.upper().tolist()  # make case-insensitive
+            m2f = m2f.loc[m2f.Factor.isin(tfs)] if whitelist else m2f.loc[~m2f.Factor.isin(tfs)]
+
+        return m2f
 
     def get_binding_score(self, motif_weights, peak_weights, outfile):
         """
         Infer TF binding score from motif z-score and peak intensity.
-        Arguments:
-            pfm {[type]} -- [motif scan result]
-            peak {[type]} -- [peak intensity]
-        Returns:
-            [type] -- [the predicted tf binding table]
         """
-        peak = dd.read_csv(peak_weights, sep="\t", blocksize=200e6)
-        pfm = dd.read_csv(motif_weights, sep="\t")
+        # merge the scoring tables
+        m = dd.read_csv(motif_weights, sep="\t")
+        m = m.merge(
+            dd.read_csv(peak_weights, sep="\t", blocksize=200e6),
+            on="region"
+        )[["motif", "region", "zscore", "log10_score"]]
+
+        # filter scoring tables for motifs found in motifs2factors
+        m = m.merge(self.motifs2factors, on="motif")  # also adds "factor" column
+
+        # combine scores
+        m = m.groupby(["factor", "region"])[["zscore", "log10_score"]].mean()
+        m = m.groupby(["region"])[["zscore", "log10_score"]].mean()
+        m = m.dropna().reset_index()
+
+        with dask.diagnostics.ProgressBar():
+            m = m.compute(num_workers=self.ncore)
 
         # Load model
         with open(self.model, "rb") as f:
             clf = pickle.load(f)
+        m["binding"] = clf.predict_proba(m[["zscore", "log10_score"]])[:, 1]
 
-        # ft = self.filtermotifs2factors
-
-        r = pfm.merge(peak, left_on="enhancer", right_on="peak")[
-            ["motif", "enhancer", "zscore", "log10_peakRPKM"]
-        ]
-        # r = r.merge(ft, left_on="motif", right_on="Motif")
-        r = r.groupby(["factor", "enhancer"])[["zscore", "log10_peakRPKM"]].mean()
-        r = r.dropna().reset_index()
-
-        with dask.diagnostics.ProgressBar():
-            table = r.compute(num_workers=self.ncore)
-        table["binding"] = clf.predict_proba(table[["zscore", "log10_peakRPKM"]])[:, 1]
-
-        table.to_csv(outfile, sep="\t", index=False)
+        m.to_csv(outfile, sep="\t", index=False)
 
     def run(self, outfile, force=False):
         if force or not os.path.exists(outfile):
