f3ef918f826a40a0c4f225710b23e5a6d4464d10
Simon van Heeringen <s.vanheeringen@science.ru.nl>
2021-06-30 14:43:08 +0200

    try other ways of limiting threads in numpy

--- ananse/__init__.py
+++ ananse/__init__.py
@@ -6,6 +6,10 @@ import os
 # 8GB of (virtual) memory. This wreaks havoc on management of the dask
 # workers.
 os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
 
 __version__ = get_versions()["version"]
 del get_versions
