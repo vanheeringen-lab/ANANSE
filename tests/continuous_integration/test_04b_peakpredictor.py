7c0324a925655315f6e60ce637fc57ecb240077c
siebrenf <siebrenf@gmail.com>
2021-11-03 22:13:22 +0100

    black

--- tests/continuous_integration/test_04b_peakpredictor.py
+++ tests/continuous_integration/test_04b_peakpredictor.py
@@ -9,9 +9,7 @@ import ananse.peakpredictor
 
 def test_peakpredictor():
     with pytest.raises(ValueError):
-        ananse.peakpredictor.PeakPredictor(
-            reference="ananse/db/default_reference"
-        )
+        ananse.peakpredictor.PeakPredictor(reference="ananse/db/default_reference")
 
     p = ananse.peakpredictor.PeakPredictor(
         reference="ananse/db/default_reference",
@@ -20,7 +18,7 @@ def test_peakpredictor():
         genome="tests/data/binding/hg38_testgenome.fa",  # 1 chr
         pfmfile="tests/data/binding/test.pfm",  # 1 motif, 1 factor
         pfmscorefile="tests/data/binding/scan.tsv",  # precomputed
-        ncore=1
+        ncore=1,
     )
 
     # boring stuff
@@ -31,8 +29,8 @@ def test_peakpredictor():
 
     # set in test.motif2factors.txt
     assert p.factors() == ["SOX12"]
-    assert p.f2m == {'SOX12': ['GM.5.0.Sox.0001']}
-    assert 'GM.5.0.Sox.0001' in p.motifs
+    assert p.f2m == {"SOX12": ["GM.5.0.Sox.0001"]}
+    assert "GM.5.0.Sox.0001" in p.motifs
 
     # parsed from input
     assert p.regions == ["chr1:1010-1020"]
@@ -41,8 +39,8 @@ def test_peakpredictor():
 
     assert p._histone_data is None
     assert "chr1:1010-1020" in p._atac_data.index
-    assert p._X_columns == ['ATAC', 'motif']
-    assert p._model_type == 'ATAC_motif'
+    assert p._X_columns == ["ATAC", "motif"]
+    assert p._model_type == "ATAC_motif"
 
     assert len(p.motif_graph) == 0  # 1 TF -> no edges
 
