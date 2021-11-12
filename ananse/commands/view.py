f30125dd68fa178746865cb5866dd1f404b43e46
siebrenf <siebrenf@gmail.com>
2021-11-12 18:58:31 +0100

    oops

--- ananse/commands/view.py
+++ ananse/commands/view.py
@@ -3,7 +3,7 @@ import sys
 
 
 def view(args):
-    df = view_h5(args.infile, args.tfs, args.regions, args.format, args.list_regions, args.list_tfs)
+    df = view_h5(args.infile, args.tfs, args.regions, args.format, args.n, args.list_regions, args.list_tfs)
 
     index = not (args.format == "long" or args.list_regions or args.list_tfs)
     header = not (args.list_regions or args.list_tfs)
